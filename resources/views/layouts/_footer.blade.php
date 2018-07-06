a7b2d54ef4ca50c079778d69f151d73a3094e9e2
snowzh <snowzh8201@outlook.com>
2018-07-06 12:46:51 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 SnowZh 设计和编码
+        </p>
+
+        <p class="pull-right"><a href="mailto:snowzh8201@outlook.com">联系我们</a></p>
+    </div>
+</footer>
